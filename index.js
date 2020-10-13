b949dace3ac24425d78a0d17fd7872d527e92a20
Aleksander Repin <alex.repin@mail.ru>
2020-10-13 22:45:09 +0300

    rebuilded and added dest to repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+	name: 'not-order',
+	paths: {
+		routes:				path.join(__dirname, 'src', 'routes'),
+		models:				path.join(__dirname, 'src', 'models'),
+	}
+};
