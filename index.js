ce255e14aa9f10d0de94ad68d9a186aa4b52a28c
Aleksander Repin <alex.repin@mail.ru>
2020-10-18 22:31:04 +0300

    added: ncOrderOptions and options initialization

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ module.exports = {
 	name: 'not-order',
 	paths: {
 		routes:				path.join(__dirname, 'src', 'routes'),
+		fields:				path.join(__dirname, 'src', 'fields'),
+		controllers:	path.join(__dirname, 'src', 'controllers'),
 		models:				path.join(__dirname, 'src', 'models'),
-	}
+	},
+	//init options and so on
+	initialize: require('./src/common/initialize.js')
 };
